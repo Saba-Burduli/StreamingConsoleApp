56da316eaa9bf576492376b29d286bd2f3b07ee1
Saba-Burduli <sabagg790@gmail.com>
2025-07-29 00:00:41 +0400

    Add initial streaming server implementation
    Introduced a streaming server using LiveStreamingServerNet with console logging enabled. The server listens for connections on port 1935 and supports asynchronous execution.

---
+++ StreamingConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using LiveStreamingServerNet;
+using FxResources.Microsoft.Extensions.Logging;
+using System.Net;
+using Microsoft.Extensions.Logging;
+
+using var server = LiveStreamingServerBuilder.Create()
+    .ConfigureLogging(options => options.AddConsole())
+    .Build();
+
+await server.RunAsync(new IPEndPoint(IPAddress.Any, 1935));
